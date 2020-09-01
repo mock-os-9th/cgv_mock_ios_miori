96b4f13a9fbcbb5b5943cbe67968aa56a134b400
iammiori <mioriii@naver.com>
2020-09-01 15:59:20 +0900

    [feat] side view 외에 남은 뷰 회색처리

--- ios/Home/SlideRight.swift
+++ ios/Home/SlideRight.swift
@@ -14,6 +14,9 @@ class SlideRight: NSObject, UIViewControllerAnimatedTransitioning {
     // 로그인 뷰가 나오면 true, 원래 홈화면이면 false
     var isPresent = false
     
+    // 남아있는 검정뷰
+    let dismissView = UIView()
+    
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 1.0
     }
@@ -33,16 +36,36 @@ class SlideRight: NSObject, UIViewControllerAnimatedTransitioning {
             // 로그인 뷰가 나와야 하는 경우
             containerView.addSubview(toViewController.view)
             
+            //dismissview 배경과 투명도
+            dismissView.backgroundColor = .black
+            dismissView.alpha = 0.0
+            
+            // dismissview 추가
+            containerView.addSubview(dismissView)
+            
+            
+            dismissView.frame = containerView.bounds
+        
+            
+            dismissView.frame.size.width = UIScreen.main.bounds.width * 0.1
+            dismissView.frame.size.height = UIScreen.main.bounds.height
+            
+  
             toViewController.view.frame = CGRect(x: toViewController.view.bounds.width, y: 0, width: finalWidth, height: finalHeight)
+            //dismsissView.bringSubviewToFront(toViewController.view)
         }
         
         // 오른쪽 -> 왼쪽 으로 슬라이드 애니메이션
         let transform = {
+            // dismissView 뿌옇게
+            self.dismissView.alpha = 0.5
             toViewController.view.transform = CGAffineTransform(translationX: -finalWidth, y: 0)
         }
         
         // dismiss 했을 때 애니메이션
         let identity = {
+            //dismissView 다시 원상태
+            self.dismissView.alpha = 0.0
             fromViewController.view.transform = .identity
         }
         
