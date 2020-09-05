6955103485564b33cdfc51d35f996bcc2cc3fe01
iammiori <mioriii@naver.com>
2020-09-05 12:18:16 +0900

    [feat] 극장선택 picker view delegate

--- ios/TheaterReservation/TheaterRViewController.swift
+++ ios/TheaterReservation/TheaterRViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-
-class TheaterRViewController: UIViewController {
+// 1. picker view protocol 을 채택할게
+class TheaterRViewController: BaseViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-//    private var viewControllers = [ UIViewController(),UIViewController()]
+    // 하나의 PickerView 안에 몇 개의 선택 가능한 리스트 표시가능한지
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //PickerView에 표시될 항목의 개수를 반환하는 메서드
+    //여기서는 우리가 보여주기로 결정한 fruits 리스트의 길이를 반환
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return bigCity.count
+    }
+    
+    //PickerView 내에서 특정한 위치(row)를 가리키게 될 때, 그 위치에 해당하는 문자열을 반환하는 메서드
+    //여기서는 fruits의 row 번째 문자열을 반환
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return bigCity[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(bigCity[row])
+    }
+
+    var bigCity = ["서울","경기","인천","강원","대전/충청","대구"]
+    @IBOutlet weak var bigCtiyPicker: UIPickerView!
+    //    private var viewControllers = [ UIViewController(),UIViewController()]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 2. protocol 준수하면서 나 대신 일할 애를 뽑을 건데 그건 self 그니까 vc 나야!
+        bigCtiyPicker.dataSource = self
+        bigCtiyPicker.delegate = self
+        
         
 //        self.dataSource = self
 //
