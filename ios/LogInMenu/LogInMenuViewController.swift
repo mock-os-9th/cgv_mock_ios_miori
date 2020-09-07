ad1fa31211b904a8350fc63f563aef2f6fa8242c
iammiori <mioriii@naver.com>
2020-09-07 16:15:43 +0900

    [feat] 로그인 메뉴 오토레이아웃 xib 파일로

--- ios/LogInMenu/LogInMenuViewController.swift
+++ ios/LogInMenu/LogInMenuViewController.swift
@@ -10,24 +10,19 @@ import UIKit
 
 class LogInMenuViewController: UIViewController {
     
-    @IBOutlet weak var fixTopView: UIView!
-    @IBOutlet weak var mainScrollView: UIScrollView!
-    @IBOutlet weak var secondTopView: UIView!
-    @IBOutlet weak var loginFiled: UIView!
-    @IBOutlet weak var grayView: UIView!
-    @IBOutlet weak var detailInfo: UIStackView!
     
-    //영화별 예매, 극장별 예매, 패스트오더, 포퇴켓 버튼 이벤트
+//    
+//    //영화별 예매, 극장별 예매, 패스트오더, 포퇴켓 버튼 이벤트
+
     @IBOutlet weak var movieR: UIStackView!
-    @IBOutlet weak var theaterR: UIStackView!
-    @IBOutlet weak var fastO: UIStackView!
-    @IBOutlet weak var photoT: UIStackView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func didTapClose(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
     
     @IBAction func pressLogin(_ sender: UIButton) {
         let loginput = LInputViewController()
@@ -50,47 +45,12 @@ class LogInMenuViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(false)
-        
-        //mainscrollview autolayout 지정
-        mainScrollView.translatesAutoresizingMaskIntoConstraints = false
-        //위치지정  x위치
-        mainScrollView.topAnchor.constraint(equalTo: fixTopView.bottomAnchor
-            ,constant: 0).isActive = true // ---- 1
-        
-        mainScrollView.heightAnchor.constraint(equalToConstant: 795)
-            .isActive = true // ---- 3
-        mainScrollView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)
-            .isActive = true // ---- 4
-        
-        secondTopView.topAnchor.constraint(equalTo: fixTopView.bottomAnchor
-            ,constant: 0).isActive = true // ---- 1
-        
-        //self.view.addSubview(self.loginFiled)
-        
-        //loginField autolayout 지정
-        loginFiled.translatesAutoresizingMaskIntoConstraints = false
-        loginFiled.topAnchor.constraint(equalTo: fixTopView.bottomAnchor
-            ,constant: 0).isActive = true // ---- 1
-        loginFiled.heightAnchor.constraint(equalToConstant: 140)
-            .isActive = true // ---- 3
-        loginFiled.heightAnchor.constraint(equalToConstant: 325)
-            .isActive = true // ---- 3
-        
-        
-        //버튼 클릭 가능하게 서브뷰를 맨앞으로 빼줘
-        //안해줬더니 버튼이 클릭이 안됨.
-        mainScrollView.bringSubviewToFront(secondTopView)
-        secondTopView.bringSubviewToFront(loginFiled)
-        secondTopView.bringSubviewToFront(detailInfo)
-        
-        
+
         //uiVIew 를 터치했을 때 이벤트 생성
         let singleTapGesture : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(movieRTap))
         //한번 누르게 할거야
         singleTapGesture.numberOfTouchesRequired = 1
         self.movieR.addGestureRecognizer(singleTapGesture)
-        
-        // Do any additional setup after loading the view.
     }
     
     @objc func movieRTap() {
@@ -102,18 +62,5 @@ class LogInMenuViewController: UIViewController {
     }
     
 }
-
-
-
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
 
 
