e673ad21b8543cfd15e1bb06d30c34f0accdd893
iammiori <mioriii@naver.com>
2020-09-08 00:21:36 +0900

    [feat] 전화번호 인증 pop up

--- ios/CJONE1/DataManager/SendPhoneAuthDataManager.swift
+++ ios/CJONE1/DataManager/SendPhoneAuthDataManager.swift
@@ -14,40 +14,62 @@ import AlamofireObjectMapper
 class SendPhoneAuthDataManager {
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
+    // code 반환해서 팝업 띄어줄거임!
     func getAuth(_ userinfo : AuthModel,_ cj1ViewController: CJ1ViewController) {
         //let uid = "miori12345"
         //let upw = "abcde12345!"
+        
         let parameter: [String:Any] = [
-
+            
             "phone" : userinfo.phoneNum,
             "phoneAgree" : true,
             "idenAgree" : true,
             "telAgree" : true,
-            "indiAgree" : true
+            "indiAgree" : false
         ]
+        
         Alamofire
             //.request("\(self.appDelegate.baseUrl)/tutorials", method: .get)
             
             //test용으로 뒤에 "/movies" 추가함 -> 성공!
             .request("\(self.appDelegate.baseUrl)/auth", method: .post, parameters: parameter, encoding: JSONEncoding.default)
-                
+            
             .validate()
             .responseObject(completionHandler: { (response: DataResponse<ResultResponse>) in
                 switch response.result {
                 case .success(let resultResponse):
                     //print("dd: \(resultResponse)")
                     if resultResponse.code == 100 {
+               
                         print("code:100 완벽한 성공띠")
+                        //팝업을 띄울거야
+                            let customAction = UIAlertAction(title: "확인", style: .destructive) { _ in
+                                print("닫음")
+                                //navigationViewController.pushViewController(MainViewController(), animated: true)
+                            }
+                        cj1ViewController.presentAlertWithAction(title: "번호인증성공", message: "회원가입버튼을 눌러주세요", customAction)
+                        
                         //mainViewController.titleLabel.text = resultResponse.message
                     } else {
+                        //팝업을 띄울거야
+                            let customAction = UIAlertAction(title: "확인", style: .destructive) { _ in
+                                print("닫음")
+                                //navigationViewController.pushViewController(MainViewController(), animated: true)
+                            }
+                        cj1ViewController.presentAlertWithAction(title: "ERROR", message: "에러발생", customAction)
                         print(resultResponse.code)
+                        
                         //mainViewController.titleLabel.text = "튜토리얼 정보를 불러오는데 실패하였습니다."
                     }
                 case .failure:
+                    //code = 444
                     print("서버와의 연결이 원할하지 않습니다")
                     //mainViewController.titleLabel.text = "서버와의 연결이 원활하지 않습니다."
                 }
             })
+        
+        
+        
     }
 }
 
