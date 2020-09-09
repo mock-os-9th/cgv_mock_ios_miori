c3f8cae377f36160476ab491ff5a66e1b2aa3d16
iammiori <mioriii@naver.com>
2020-09-09 19:59:34 +0900

    [feat] 영화예매 바텀시트 - 인원선택 뷰

---
+++ ios/SheetView/controller/ReserViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ReserViewController.swift
+//  ios
+//
+//  Created by miori Lee on 2020/09/09.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class ReserViewController: UIViewController {
+    
+  
+    @IBAction func didTap(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+ 
+    @IBAction func didSelect(_ sender: UIButton) {
+        print("tap")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.isHidden = true
+        
+        // Do any additional setup after loading the view.
+        
+        //Dispatch Queue를 이용해 bottom sheet 1초 후에 띄우기
+        let seconds = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Put your code which should be executed with a delay here
+            //print("hello")
+            self.view.isHidden = false
+            //self.adImage2.isHidden = false
+        
+        }
+        
+        //peopleSelect.isHidden = true
+    }
+}
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
