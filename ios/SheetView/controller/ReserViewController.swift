e4e12b4d43d09b5f201f4edd881c9be6dc805090
iammiori <mioriii@naver.com>
2020-09-12 00:31:12 +0900

    [feat] 좌석선택뷰로 넘기는 함수추가

--- ios/SheetView/controller/ReserViewController.swift
+++ ios/SheetView/controller/ReserViewController.swift
@@ -8,19 +8,32 @@
 
 import UIKit
 
-class ReserViewController: UIViewController {
+class ReserViewController: BaseViewController {
     
-  
+    
     @IBAction func didTap(_ sender: UIButton) {
         self.dismiss(animated: false, completion: nil)
     }
- 
+    
     @IBAction func didShare(_ sender: Any) {
         print("share")
     }
     @IBAction func didSelect(_ sender: UIButton) {
-        print("hello")
+
+        // 현재 뷰는 날리고 좌석 선택뷰를 띄워주기
+        let seatVC = UINavigationController(rootViewController: SelectSeatViewController(nibName: "SelectSeatViewController", bundle: nil))
+        seatVC.modalPresentationStyle = .fullScreen
+        
+        guard let pvc = self.presentingViewController else { return }
+        
+        self.dismiss(animated: false) {
+            pvc.present(seatVC, animated: true, completion: nil)
+        }
+        
+        
+        
     }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +48,7 @@ class ReserViewController: UIViewController {
             //print("hello")
             self.view.isHidden = false
             //self.adImage2.isHidden = false
-        
+            
         }
         
         //peopleSelect.isHidden = true
