2010047a6211d87633035abbd066d47f854b300e
iammiori <mioriii@naver.com>
2020-09-12 00:37:21 +0900

    [feat] 콜렉션뷰 투명하게, api연동 성공

--- ios/TheaterReservation/TheaterRMainViewController.swift
+++ ios/TheaterReservation/TheaterRMainViewController.swift
@@ -9,12 +9,7 @@
 import UIKit
 
 
-
-
-
-
 class TheaterRMainViewController: BaseViewController {
-    
     
     
     @IBOutlet weak var collectionView: UICollectionView!
@@ -23,8 +18,10 @@ class TheaterRMainViewController: BaseViewController {
     let collecionCell = "moviePosterCell"
     let collecionCell2 = "TimeSeatCollectionViewCell"
   // cell 이미지 데이터
-    //var moviePoster : [MoviePosterModel] = [MoviePosterModel(imageURL: "")]
-    var moviePoster : [String] = ["https://ssl.pstatic.net/imgmovie/mdi/mit110/1900/190010_P02_162156.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1890/189069_P23_111151.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1822/182234_P49_102023.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1837/183772_P08_181006.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0319/31940_P46_141843.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1870/187051_P13_182257.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1896/189618_P15_163953.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1943/194313_P10_112905.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1937/193799_P13_150412.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1823/182353_P11_135005.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1586/158617_P14_181605.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1896/189623_P07_135856.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1893/189373_P04_104626.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1676/167622_P04_111001.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1889/188909_P22_102659.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0525/52515_P98_100506.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1859/185917_P28_182718.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1950/195061_P09_101025.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1776/177608_P11_182934.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1783/178351_P22_132611.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1903/190355_P16_100056.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0103/10323_P04_142631.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1904/190458_P05_102228.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1831/183116_P01_154806.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1664/166415_P07_164358.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0109/10922_P12_150601.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0804/80466_P50_105129.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1933/193330_P14_182748.jpg", "none", "none", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1714/171477_P00_142258.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1798/179859_P19_104011.jpg", "none", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1864/186487_P18_160126.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1786/178687_P13_102523.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0363/36331_P04_173243.jpg", "none", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0136/13636_P25_151053.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1742/174294_P13_155544.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1954/195425_P09_172300.jpg", "none", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1348/134824_P20_164819.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1766/176625_P14_111512.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0311/31130_P04_150024.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0150/15006_P17_101432.jpg", "https://ssl.pstatic.net/imgmovie/mdi/mit110/1851/185139_P14_134413.jpg", "none", "https://ssl.pstatic.net/imgmovie/mdi/mit110/0106/10676_P18_111546.jpg", "none"]
+    // 요거는 데이터 모델 테스트 (데이터 모델 했을때  set으로 접근이 되서 어려움 ㅜ)
+    //var moviePoster2 : [MoviePosterModel] = [MoviePosterModel(imageURL: "")]
+    var moviePoster : [String] = [String]()
+    //collecion view 이미지 잘 들어가나 테스트
     
     //    private var moviePoster: [UIImage] {
     //        var moviePoster:[UIImage] = []
@@ -47,6 +44,8 @@ class TheaterRMainViewController: BaseViewController {
         super.viewDidLoad()
         //TimeSeatCollectionViewCell
         
+        //collectionView.reloadData()
+        
         
         //포스터 콜렉션 뷰
         collectionView.delegate = self
@@ -54,7 +53,7 @@ class TheaterRMainViewController: BaseViewController {
         collectionView.register(UINib(nibName: "MovieCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "MovieCollectionViewCell")
         
         // 콜렉션뷰 투명하게
-        collectionView.backgroundView = nil;
+        collectionView.backgroundView = nil
         collectionView.backgroundColor = .clear
         
         //영화시간, 좌석 콜렉션뷰
@@ -67,21 +66,23 @@ class TheaterRMainViewController: BaseViewController {
         // collection view tag 지정
         collectionView.tag = 0
         timeseatcollecionView.tag = 1
-        
-        //GetPosterDataManager().getPosterImage(self)
-        
-        
-        
+
+
         //datamanager 연결
-  
         GetPosterDataManager().getPosterImage(self)
+        //collectionView.reloadData()
         
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+        print("view will appear")
+        //collectionView.reloadData()
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(false)
-        
-        //print("vc : \(moviePoster)")
-        
+        // GetPosterDataManager().getPosterImage(self)
+        print("view Did Appear")
+
     }
     
     
@@ -114,10 +115,12 @@ extension TheaterRMainViewController: UICollectionViewDelegate, UICollectionView
                 cell.moviePoster.image = UIImage(data: converImage as Data)
             }
             
+            
             return cell
         } else {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeSeatCollectionViewCell", for: indexPath) as? TimeSeatCollectionViewCell else {
                 return TimeSeatCollectionViewCell()
+
             }
             //cell.moviePoster.i
             
