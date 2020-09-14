e09cc99b1ea4f9839e139a91cf36cf1a9182b0bc
iammiori <mioriii@naver.com>
2020-09-15 07:07:11 +0900

    [feat] 영화상세뷰컨트롤러

---
+++ ios/DetailMovie/DetailMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailMovieViewController.swift
+//  ios
+//
+//  Created by miori Lee on 2020/09/15.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class DetailMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
