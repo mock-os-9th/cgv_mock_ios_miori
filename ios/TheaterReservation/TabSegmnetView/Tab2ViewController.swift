faafb1433ad70c48278cdf9f1509d44a6ce3c65b
iammiori <mioriii@naver.com>
2020-09-18 00:21:47 +0900

    [feat] 탭 페이지 컨트롤러 추가

---
+++ ios/TheaterReservation/TabSegmnetView/Tab2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Tab2ViewController.swift
+//  ios
+//
+//  Created by miori Lee on 2020/09/17.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class Tab2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
